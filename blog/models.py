0cacfb0a252b230a264869f428f5cf9e165df4d8
zaid kamil <xaidmetamorphos@gmail.com>
2024-08-14 14:01:13 +0530

    template inheritance and model creation

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length = 150)
+    content = models.TextField()
+    image = models.ImageField(upload_to="articles/")
+    created_at = models.DateTimeField(auto_now_add=True) # autofill when created
+    
+    
+    
+    
--- main/views.py
+++ main/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
+
     return render(request, 'index.html')
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
     return render(request, 'login.html')
 
 def register_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+        if password == cpassword:
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect('login')
     return render(request, 'register.html')
 
 def logout_view(request):
+    logout(request)
     return redirect('index')
