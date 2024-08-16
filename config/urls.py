d457f3c31f806787b1f33f8afdd8e51a88af7608
zaid kamil <xaidmetamorphos@gmail.com>
2024-08-16 13:10:39 +0530

    articles

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # blog views
     path('blog/all', blog_views.blog_list_view, name='blog_list'),
     path('blog/<int:id>/article', blog_views.article_view, name='article'),
-    path('blog/<int:id/edit', blog_views.edit_article_view, name='edit_article'),
+    path('blog/<int:id>/edit', blog_views.edit_article_view, name='edit_article'),
     path('blog/add', blog_views.add_article_view, name='add_article'),
     path('blog/<int:id>/delete', blog_views.delete_article_view, name='delete_article'),
 ]
