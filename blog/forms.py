f22ce129cdb6e5a535cc8c87d466dd748542cff8
zaid kamil <xaidmetamorphos@gmail.com>
2024-08-23 14:21:56 +0530

    add article and display details

---
+++ blog/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['image', 'title', 'content']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),  
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Article
-# Create your views here.
-def blog_list_view(request):
+from .forms import ArticleForm
 
+def blog_list_view(request):
     return render(
         request,
         'blog_list.html',
@@ -14,19 +14,45 @@ def blog_list_view(request):
 def article_view(request, id):
     return render(
         request,
-        'article.html'
+        'article.html',
+        context={
+            'article' : Article.objects.get(id=id)
+        }
     )
 
 def edit_article_view(request, id):
+    article = Article.objects.get(id=id)        # Get the article
+    form = ArticleForm(instance=article)        # create form with article
+    if request.method == 'POST':                # if form is submitted
+        form = ArticleForm(request.POST, request.FILES, instance=article) # create form with submitted data
+        if form.is_valid():                     # if form is valid
+            article = form.save(commit=False)   # save form data to article
+            article.author = request.user       # set author to current user
+            article.save()                      # save article
+            return redirect('blog_list')        # redirect to blog list
     return render(
         request,
-        'edit_article.html'
+        'edit_article.html',
+        context = {
+            'form': form
+        }
     )
 
 def add_article_view(request):
+    form = ArticleForm()
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('blog_list')
     return render(
         request,
-        'add_article.html'  
+        'add_article.html',
+        context = {
+            'form': form
+        }
     )
 
 def delete_article_view(request, id):
